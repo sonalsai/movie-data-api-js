859ca7ce4455ba72984a20655b581ed76772abd9
Sonal Sai K S <sonalsai777@gmail.com>
2024-07-17 21:14:25 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,80 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const searchInput = document.querySelector("#searchInput");
+  const searchBtn = document.getElementById("searchBtn");
+  const errorMessage = document.querySelector(".errorMessage");
+  const movieContainer = document.querySelector(".movieContainer");
+  let movieName = "";
+
+  const getMovieData = async () => {
+    movieName = searchInput.value;
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NTZkYjU4YmVlMThkYjZhYWE1ODMzNGYxNjcyNDk3NSIsIm5iZiI6MTcyMTIwOTY2MC45NjE2MzIsInN1YiI6IjY2OTc5MThlN2ZjOWViNGZhMjY2ZGI4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.doznP4_cy1tXwTxD21gR8eflNnBefLiiawSKqgE0oas",
+      },
+    };
+
+    if (movieName) {
+      errorMessage.style.visibility = "hidden";
+      try {
+        const response = await fetch(
+          `https://api.themoviedb.org/3/search/movie?query=${movieName}`,
+          options
+        );
+        const data = await response.json();
+        console.log("movieee", data.results);
+        renderMovies(data.results);
+      } catch (error) {
+        console.log("Error : ", error);
+      }
+    } else {
+      errorMessage.style.visibility = "visible";
+      movieContainer.textContent = "";
+    }
+  };
+
+  const createElement = (movie) => {
+    const movieDiv = document.createElement("div");
+    movieDiv.className = "movie";
+
+    const movieImage = document.createElement("img");
+    movieImage.className = "movieImage";
+    movieImage.src = `https://image.tmdb.org/t/p/original${movie.poster_path}`;
+
+    const movieData = document.createElement("div");
+    movieData.className = "movieData";
+
+    const movieTitle = document.createElement("h1");
+    movieTitle.className = "movieTitle";
+    movieTitle.textContent = movie.original_title;
+
+    const movieReleaseDate = document.createElement("h3");
+    movieReleaseDate.className = "movieReleaseDate";
+    movieReleaseDate.textContent = `Release Date : ${movie.release_date}`;
+
+    const movieOverview = document.createElement("p");
+    movieOverview.className = "movieOverview";
+    movieOverview.textContent = movie.overview;
+
+    movieData.append(movieTitle);
+    movieData.append(movieReleaseDate);
+    movieData.append(movieOverview);
+
+    movieDiv.append(movieImage);
+    movieDiv.append(movieData);
+    movieContainer.append(movieDiv);
+  };
+
+  const renderMovies = (movies) => {
+    movieContainer.textContent = "";
+    movies?.map((movie) => {
+      createElement(movie);
+    });
+  };
+
+  searchBtn.addEventListener("click", () => {
+    getMovieData();
+  });
+});
